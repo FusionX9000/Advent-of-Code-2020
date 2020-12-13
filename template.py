ae8fb7150d0fc584e7491d7aec1f90ae51e3f7db
Salman Khan <2805979+FusionX9000@users.noreply.github.com>
2020-12-14 03:09:52 +0530

    template for each solution

---
+++ template.py
@@ -0,0 +1,17 @@
+def part1(data):
+    return
+
+
+def part2(data):
+    return
+
+
+def process_input(file):
+    return []
+
+
+if __name__ == "__main__":
+    with open('../inputs/DayN.txt', 'r') as f:
+        data = process_input(f)
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
